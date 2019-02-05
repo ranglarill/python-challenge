f021a5b9cf8c96e1981ccc046cfc82da977ed760
Rodrigo Anglarill <ranglarill@Rodrigos-MacBook-Pro.local>
2019-02-05 17:38:24 -0500

    updates to files

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+#path to collect the data from file
+bankCSV = os.path.join('..', 'Resources', 'budget_data.csv')
+#define the function and make 'bankData' the sole parameter
+
